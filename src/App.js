e810ee04a169d817fe08e8d1fdacb503d15ee605
ranjan-maji <99948732+ranjan-maji@users.noreply.github.com>
2022-10-13 17:18:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import PreNavbar from "./components/PreNavbar";
+import Navbar from "./components/Navbar";
+import {BrowserRouter as Router} from "react-router-dom"
+import Slider from "./components/Slider.js";
+function App() {
+  return (
+    <Router>
+      <PreNavbar />
+      <Navbar/>
+      <Slider/>
+    </Router>
+  );
+}
+
+export default App;
